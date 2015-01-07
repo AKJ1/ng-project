a7a2573cbc54389f71f6837f4be935a18af1a47b
akj1 <kellerr@abv.bg>
2015-01-08 00:09:56 +0200

    Finished filtering and pagination, added more styles.

--- js/controllers/homeCtrl.js
+++ js/controllers/homeCtrl.js
@@ -1,42 +1,67 @@
 app.controller('homeCtrl', ['$scope', 'ClassifiedsFactory', 'TownsFactory', 'CategoriesFactory', function($scope, ClassifiedsFactory, TownsFactory, CategoriesFactory) {
 	$scope.pageTitle = "HomePage";
-	
-
-	ClassifiedsFactory.getAds()
-	.$promise
-	.then(
-	function(data){
-        $scope.classifieds = data;
-	});
-
+	$scope.currentCategory = 0;
+	$scope.currentTown = 0;
+	$scope.currentPage = 1;
 
 	TownsFactory.getTowns()
 	.$promise
 	.then(
-			function(data) {				
-				$scope.towns = data;
+		function(data) {				
+			$scope.towns = data;
 	});
-
 
 	CategoriesFactory.getCategories()
 	.$promise
 	.then(
-		function(data) {				
+		function(data) {	
 			$scope.categories = data;
 	});
 
-	function filterByCategory(){
-		console.log("filtering");
-	}
+	$scope.getCategoryById = function(categoryId){
+		for(var category in $scope.categories){
+			if ($scope.categories[category].id == categoryId) {
+				return $scope.categories[category];
+			}
+		}
+	};
+	$scope.getTownById = function(townId){
+		for(var town in $scope.towns){
+			if ($scope.towns[town].id == townId) {
+				return $scope.towns[town];
+			}
+		}
+	};
 
-	$scope.filterByCategory = function(category){
-		console.log("filtering");
-		$scope.classifieds = {};
-		ClassifiedsFactory.getAdsByCategory(category)
+	$scope.changeCategory = function(categoryId){
+        $scope.currentPage = 1;
+       	$scope.currentCategory = categoryId;
+       	$scope.filter();
+	};
+	$scope.changeTown = function(townId){
+        $scope.currentPage = 1;
+        $scope.currentTown = townId;
+        $scope.filter();
+	};
+	$scope.nextPage = function(){
+		$scope.currentPage += 1;
+		$scope.filter();
+	};
+	$scope.prevPage = function(){
+		$scope.currentPage -= 1;
+		$scope.filter();
+	};
+
+	$scope.filter = function(){
+		ClassifiedsFactory.getAdsWithFilters($scope.currentCategory, $scope.currentTown, $scope.currentPage)
 		.$promise
 		.then(
-			function(data){
+			function(data){				
 				$scope.classifieds = data;
+			},
+			function(error){
+				console.log(error);
 			});
 	};	
+	$scope.filter();
 }]);
--- js/services/data/classifiedsFactory.js
+++ js/services/data/classifiedsFactory.js
@@ -5,9 +5,13 @@ app.factory('ClassifiedsFactory', ['$resource', 'baseServiceUrl', function($reso
 		var resource = $resource(adsUrl);
 		return resource.get();
 	};
-	factory.getAdsByCategory = function(categoryId){
-		var resource = $resource(adsUrl, {"categoryid": categoryId});
-		return resource.query();
+	factory.getAdsWithFilters = function(categoryId, townId, page){
+		var categoryFilter = categoryId ? "?categoryid=" + categoryId : "";
+		var townFilter = townId ?(categoryId ? "&" : "?") + "townId=" + townId : ""; 
+		var pageFilter = ((townId || categoryId) ? "&" : "?") + "startpage=" + page;
+		var pageLengthFilter = "&pagesize=6";
+		var resource = $resource(adsUrl + categoryFilter + townFilter + pageFilter + pageLengthFilter);
+		return resource.get();
 	};
 	return factory;
 }]);
