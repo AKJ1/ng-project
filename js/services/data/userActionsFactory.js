ecb8ee3fe4d3b8660c2415d40350cf673d716bf7
akj1 <kellerr@abv.bg>
2015-01-09 07:43:49 +0200

    added the unadded user actions

--- js/services/data/userActionsFactory.js
+++ js/services/data/userActionsFactory.js
@@ -53,7 +53,7 @@ app.factory('userActionFactory', ['baseServiceUrl','$http',function ($http) {
 			return(data);
 		});
 	};
-	factory.newUserAd = function(title,text,changeImage,imageDataUrl,categoryId,townId,classifiedId){
+	factory.newUserAd = function(title,text,imageDataUrl,categoryId,townId,classifiedId){
 		var requestObject = {
 			headers: {
 				'Authorization': 'Bearer ' + sessionStorage.loginToken
@@ -71,8 +71,16 @@ app.factory('userActionFactory', ['baseServiceUrl','$http',function ($http) {
 			return(data);
 		});
 	};
-	factory.getAdById = function(){
-
+	factory.getAdById = function(classifiedId){
+		var requestObject = {
+			headers: {
+				'Authorization': 'Bearer ' + sessionStorage.loginToken
+			},
+		};
+		$http.post(baseServiceUrl + 'user/ads/' + classifiedId, requestObject)
+		.then(function(data){
+			return(data);
+		});
 	};
 	factory.deleteUserAd = function(classifiedId){
 		var requestObject = {
@@ -85,6 +93,51 @@ app.factory('userActionFactory', ['baseServiceUrl','$http',function ($http) {
 			return(data);
 		});
 	};
+	factory.getUserProfile = function(){
+		var requestObject = {
+			headers: {
+				'Authorization': 'Bearer ' + sessionStorage.loginToken
+			}
+		};
+		$http.get(baseServiceUrl + 'user/profile', requestObject)
+		.then(function(data){
+			return(data);
+		});
+	};
+	factory.editUserProfile = function(name, email, phonenumber, townid){
+		var requestObject = {
+			headers: {
+				'Authorization': 'Bearer ' + sessionStorage.loginToken
+			},
+			data:{
+				'name': name,
+				'email': email,
+				'phonenumber': phonenumber,
+				'townid': townid
+			}
+		};
+		$http.put(baseServiceUrl + 'user/profile', requestObject)
+		.then(function(data){
+			return(data);
+		});
+	};
+	factory.changeUserPassword = function(oldpass, newpass, repeatNewpass){
+		var requestObject = {
+			headers: {
+				'Authorization': 'Bearer ' + sessionStorage.loginToken
+			},
+			data:{
+				'oldPassword': oldpass,
+				'newPassword': newpass,
+				'confirmPassword': repeatNewpass
+			}
+		};
+		$http.put(baseServiceUrl + 'user/profile', requestObject)
+		.then(function(data){
+			return(data);
+		});
+	};
+
 
 	return factory;
 }]);
