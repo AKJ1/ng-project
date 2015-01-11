c38ba307d3e640ec334b1e09e53d1ca4d3af9355
akj1 <kellerr@abv.bg>
2015-01-11 02:37:14 +0200

    Added towns & categories editing layout

--- js/controllers/admin/adminTownCtrl.js
+++ js/controllers/admin/adminTownCtrl.js
@@ -1,3 +1,7 @@
 app.controller('adminTownCtrl', ['$scope', '$location', 'AdminActionsFactory', function($scope, $location, AdminActionsFactory){
-	
+
+	AdminActionsFactory.getTowns(function(response){
+		console.log(response);
+		$scope.towns = response.data.towns;
+	});
 }]);
